13281548e4e29bfe508f49d9c4ff665130776fed
Admin <admin@hotmail.com>
2021-03-20 18:25:40 +0100

    first view done

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Controller;
-
-use App\Form\ConnexionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
+
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+
+
+use App\Entity\Etudiant;
+//use App\Form\ConnexionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -42,6 +50,58 @@ class SecurityController extends AbstractController
 
     public function deconnexion(){
 
+    }
+    /**
+     * @Route ("/etudiant/new",name="etudiant_create")
+     */
+
+    public function create(Request $request,EntityManagerInterface $manager){
+        $etudiant= new Etudiant();
+
+        $form=$this->createFormBuilder($etudiant)
+            ->add('numEtudiant')
+            //->add('Utilisateur')
+            ->add('nom')
+            ->add('prenom')
+            ->add('DNN')
+            ->add('RSE')
+            ->add('redoublant')
+            ->add('tiersTemps')
+            ->add('ajac')
+            ->add('semestreObtenu')
+            ->add('adresse')
+            ->add('email')
+            ->add('enregistrer',SubmitType::class,[
+                'label'=>'Enregistrer'
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+        dump($etudiant);
+
+        if($form->isSubmitted() && $form->isValid()) {
+        $manager->persist($etudiant);
+        $manager->flush();
+
+        return $this->redirectToRoute('etudiant_show',['id'=>
+        $etudiant->getId()
+
+        ]);
+        }
+
+        return $this->render('site/create.html.twig', [
+            'formEtudiant'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @Route ("/etudiant/{id}",name="etudiant_show")
+     */
+
+    public function show(Etudiant $etudiant){
+        return $this->render('site/show.html.twig',[
+            'etudiant'=>$etudiant
+        ]);
     }
 
 }
